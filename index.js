3eb2ae9fd3ecd3c68bf829160b9677d6fdc70232
Akshay Kumar <136438757+akshayadv@users.noreply.github.com>
2024-04-21 20:03:00 +0530

    reload btn added

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ const projectIntro = `<div class="project-introduction">
                 </p>
             </div>`;
 
+
+const reloadSvgIcon = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="" height="1.4rem" width="1.4rem" version="1.1" id="Layer_1" viewBox="0 0 367.136 367.136" xml:space="preserve">
+<path d="M367.136,149.7V36.335l-39.14,38.953c-13.024-17.561-29.148-32.731-47.732-44.706  c-29.33-18.898-63.352-28.888-98.391-28.888C81.588,1.694,0,83.282,0,183.568s81.588,181.874,181.874,181.874  c34.777,0,68.584-9.851,97.768-28.488c28.394-18.133,51.175-43.703,65.881-73.944l-26.979-13.119  c-25.66,52.77-78.029,85.551-136.669,85.551C98.13,335.442,30,267.312,30,183.568S98.13,31.694,181.874,31.694  c49.847,0,96.439,24.9,124.571,65.042L253.226,149.7H367.136z"/>
+</svg>`;
+
+
 // Model
 class Tab {
     constructor(url = '', id = 1) {
@@ -64,24 +70,36 @@ class TabView {
         const content = this.tabContents[ id ];
         if (content) {
             this.contentContainer.html(content);
+            $('.search-field').val(tab.url);
         } else {
             this.loadTabContent(tab);
         }
 
-
         if (!tab.url) {
             $('.content-container').html(projectIntro);
         }
+
+        $('.reload-icon').on('click', () => {
+            this.reloadTab();
+        });
     }
 
     closeTab(tab) {
         this.controller.closeTab(tab);
     }
 
+    createContentContainerChunk(tab) {
+        return `<div class="search-container-wrapper">
+                    <div class="reload-icon">
+                        ${reloadSvgIcon}
+                    </div>
+                    <input class="search-field" type="text" />
+                </div>
+                <div class="content-container"></div>`;
+    }
+
     loadTabContent(tab) {
-        const content = `<div class="search-container-wrapper">
-            <input class="search-field" type="text"></div>
-            <div class="content-container"><iframe src="${tab.url}" width="100%" height="400"></iframe></div>`;
+        const content = this.createContentContainerChunk(tab);
         this.tabContents[ tab.id ] = content;
         this.contentContainer.html(content);
 
@@ -89,9 +107,15 @@ class TabView {
         if (inputField.length === 1) {
             inputField.focus();
         }
-
-        $('.content-container').html(projectIntro);
     }
+
+    reloadTab() {
+        const tabId = this.controller.activeTabId;
+        const srcValue = this.controller.activeTab.url;
+        const iframeId = `iframe-${tabId}`;
+        $(`#${iframeId}`).attr('src', srcValue);
+    }
+
 }
 
 // Controller
@@ -99,7 +123,8 @@ class TabController {
     constructor() {
         this.tabs = [];
         this.view = new TabView(this);
-        this.activeTab = null;
+        this.activeTabId = null;
+        this.activeTab = {};
     }
 
     addTab(url) {
@@ -113,11 +138,14 @@ class TabController {
         }
         this.tabs.push(newTab);
         this.view.addTab(newTab);
+        this.activeTab = newTab;
         this.switchTab(id);
     }
 
     switchTab(tabId) {
-        this.activeTab = tabId;
+        if (this.activeTabId === tabId) return;
+
+        this.activeTabId = tabId;
         this.view.switchTab(tabId);
     }
 
@@ -132,18 +160,19 @@ class TabController {
         if (this.tabs.length === 0) {
             this.view.tabContents = {};
             this.view.contentContainer.html('');
-            this.activeTab = null;
+            this.activeTab = {};
+            this.activeTabId = null;
         }
 
-        if (this.activeTab === tab.id) {
+        if (this.activeTabId === tab.id) {
             this.activeTab = this.tabs[ tabIndex ] || this.tabs[ tabIndex - 1 ];
+            this.activeTabId = this.activeTab.id;
             this.switchTab(this.activeTab.id);
         }
 
     }
 }
 
-// Initialize
 $(document).ready(() => {
     const controller = new TabController();
     controller.addTab('');
@@ -155,13 +184,17 @@ $(document).ready(() => {
     $(document).on('keydown', '.search-field', function (event) {
         if (event.key === "Enter" || event.keyCode === 13) {
             const value = $(this).val();
-            const container = $(this).closest('#content-container-wrapper').find('.content-container');
-            container.html(`<iframe src="${value}"></iframe>`);
-            const activeTabId = controller.activeTab;
-            controller.view.tabContents[ activeTabId ] = container.html();
+            const activeTabId = controller.activeTabId;
+            const activeTab = controller.tabs.find(tab => tab.id === activeTabId);
+            activeTab.url = value;
+            const parentContainer = $('#content-container-wrapper');
+            const container = parentContainer.find('.content-container');
+            container.html(`<iframe src="${value}" id="iframe-${activeTabId}"></iframe>`);
+            controller.view.tabContents[ activeTabId ] = parentContainer.html();
             $(this).blur();
         }
     });
+
 
     $('.minimize-window').on('click', function () {
         $('#custom-browser-container').toggleClass('hidden');
